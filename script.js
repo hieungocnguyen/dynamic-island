2c0f79543ac79d3894c5b56d945d35b2b0f69b0a
hieungocnguyen <ngnohieu@gmail.com>
2024-07-02 23:35:32 +0700

    test gsap

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Create the animation using GSAP
-gsap.to(".box", { 
-  duration: 2, 
-  x: 300, 
-  rotation: 360, 
-  scale: 1.5, 
-  backgroundColor: "#e74c3c", 
-  ease: "power2.inOut"
+const box = document.querySelector(".box");
+
+box.addEventListener("click", () => {
+  gsap.to(".box", { 
+    duration: 0.6,
+    width:200,
+    ease: "power2.inOut"
+  });
 });
